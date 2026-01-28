a78e1d3e3e8647de485cd351dfd3ceed621198a3
ESheps <eitan.sheps@gmail.com>
2026-01-28 19:03:46 +0200

    Final: Hybrid build for bypass

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const path = require('path');
 const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.get('/api/handshake-proxy', async (req, res) => {
     try {
